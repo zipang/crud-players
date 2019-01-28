a067c0aadb2810be4043c803b6b74b60f3f1614d
zipang <christophe.desguez@gmail.com>
2019-01-28 13:55:36 +0100

    Added more context to the validation tests

--- src/models/players/index.spec.js
+++ src/models/players/index.spec.js
@@ -4,7 +4,7 @@ const { expect } = require("code");
 
 describe("Model validation", function() {
 
-	it(`A new Player object should be created with valid data`, function() {
+	it(`A new Player validating proxy should be return by the factory on the sample data`, function() {
 		const examples = schema.examples;
 		examples.forEach(data => {
 			expect(create(data)).to.equal(data);
@@ -12,15 +12,18 @@ describe("Model validation", function() {
 		//expect.assertions(examples.length);
 	});
 
-	it(`A Player object can only be modified with valid data`, function() {
+	describe(`An object returned by the factory can only be modified with valid data`, function() {
 
 		const Player = create(schema.examples[0]);
 
-		// Changing for another value of the same type is allowed
-		Player.lastName = 'X';
-		expect(Player.lastName).to.equal('X');
+		it("Setting a property to another value of the same type is allowed", function() {
+			Player.lastName = 'X';
+			expect(Player.lastName).to.equal('X');
+		});
 
-		expect(() => { Player.firstName = 42 }).to.throw(ValidationError);
+		it ("Setting a wrong data-type on a property will automatically throw", function() {
+			expect(() => { Player.firstName = 42 }).to.throw(ValidationError);
+		});
 
 	});
 })
