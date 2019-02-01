653e2f245baf8b357ac4b800b85a62031373ace5
zipang <christophe.desguez@gmail.com>
2019-02-01 08:51:29 +0100

    Trying an HTTP unit test with micro and test-listen

--- src/api/players/create.spec.js
+++ src/api/players/create.spec.js
@@ -2,21 +2,29 @@ const micro = require("micro");
 const listen = require("test-listen");
 const createService = require("./create");
 const fetch = require("node-fetch");
+const { expect } = require("code");
 
-describe("CREATE API entry point", async function() {
+describe("CREATE API entry point", function() {
+
 	const service = micro(createService);
-	const testUrl = await listen(service);
 
-	it("Respond to POST requests", async function() {
+	it("Respond with a newly created object with an id on POST requests", async function() {
+
+		const testUrl = await listen(service);
+
 		const resp = await fetch(testUrl, {
 			method: "POST",
 			headers: {
-				"Content-Type": "application/json"
+				Accept: "application/json",
+				"Content-Type": "application/json",
 			},
-			body: {
-				firstName: "John"
-			}
+			body: JSON.stringify({
+				firstName: "John",
+			}),
 		});
 
+		console.dir(resp.body);
+
+		expect(resp.body).to.contain(["id", "firstName"]);
 	});
 });
