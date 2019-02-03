09c4b136a787e5cb8bcfe2dc91f67b81fcd7ae3e
zipang <christophe.desguez@gmail.com>
2019-02-03 17:04:13 +0100

    Export the validate method

--- src/models/index.js
+++ src/models/index.js
@@ -2,33 +2,51 @@ const ValidationError = require('./ValidationError');
 
 const models = {};
 
+const getModel = (domain) => {
+
+	if (!models[domain]) {
+		try {
+			// instanciate the factory
+			models[domain] = require(`./${domain}/`);
+		} catch(err) {
+			throw new Error(`${domain} model doesn't exist`);
+		}
+	}
+	return models[domain];
+}
+
 /**
- * Models factory
+ * Model factory
  * Usage example :
- *   const Models = require('./Models/');
- *   let newTodo = Models.create('todos', { id: 'lets-do-it', content: 'Let s do It !' });
- * @param  {String} modelName
+ *   const { create } = require('./models/');
+ *   let newTodo = create('todos', { id: 'lets-do-it', content: 'Let s do It !' });
+ * @param  {String} domain - name of
  * @param  {Object} data
- * @returns {Object} instance of modelName hydrated with passed data
+ * @returns {Object} instance of domain hydrated with passed data
  * @throws {ValidationError} If the data doesn't conform to the model's schema
  */
-const create = (modelName, data) => {
-
-	if (!models[modelName]) {
-		// instanciate the factory
-		models[modelName] = require(`./${modelName}/`).create;
-	}
+const create = (domain, data) => {
 
 	try {
-		let newInstance = models[modelName](data);
-		return newInstance;
+		return getModel(domain).create(data);
 	} catch (err) {
 		if (err instanceof ValidationError) {
 			throw err;
 		} else {
-			throw new ValidationError(modelName, data, err.message);
+			throw new ValidationError(domain, data, err.message);
 		}
 	}
 }
 
-module.exports = { create };
+/**
+ * Retrieves only the validate method of a domain
+ * @param  {String} domain - name of
+ * @returns {Function} validate method
+ * @throws {DomainError} If the data doesn't conform to the model's schema
+ */
+const validate = (domain) => {
+
+	return getModel(domain).validate;
+}
+
+module.exports = { create, validate };
