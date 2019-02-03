fc9785ef7af5d901a60bf9ac37a441684237465c
zipang <christophe.desguez@gmail.com>
2019-02-03 22:03:52 +0100

    ADDED the validation to the stores

--- src/api/store.js
+++ src/api/store.js
@@ -1,17 +1,28 @@
-const FaunaDbStore = require("../store/faunadb");
-const InmemoryDbStore = require("../store/in-memory");
+const createStore  = require("../store");
+const { validate } = require("../models");
 const StorageError = require("../store/StorageError");
 
 /**
  * Return the store to be used depending of the environment variables
  */
 module.exports = (collectionName) => {
+
+	// Retrieve the validation method linkde to this model
+	const validateModel = validate(collectionName);
+
 	if (!process.env.PLAYERS_API_SECRET) {
 		console.log(`Delivering an in-memory data store for ${collectionName}`);
-		return InmemoryDbStore.create(collectionName);
+		return createStore(collectionName, {
+			db: "in-memory",
+			validate: validateModel
+		});
 	} else {
-		return FaunaDbStore.create(collectionName, {
-			secret: process.env.PLAYERS_API_SECRET,
+		return createStore(collectionName, {
+			db: "faunadb",
+			conf: {
+				secret: process.env.PLAYERS_API_SECRET,
+			},
+			validate: validateModel
 		});
 	}
 };
