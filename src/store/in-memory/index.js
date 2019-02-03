5ef2e860e98022b50a8491683360fb973cca00a9
zipang <christophe.desguez@gmail.com>
2019-02-03 16:59:19 +0100

    Removed the validation layer from the store's implementation

--- src/store/in-memory/index.js
+++ src/store/in-memory/index.js
@@ -1,39 +1,25 @@
 /**
  * Creates a new in-memory data store
+ * @param {String} domain
  * @param {Object} conf
  * @return {Store}
  */
-const create = (domain, conf) => {
+module.exports = (domain, conf) => {
 	const map = new Map();
 
 	const store = {
 		get: (key) => map.get(key),
 		has: (key) => map.has(key),
-		set: (key, value) => map.set(key, value),
+		create: (data) => {
+			data.id = (Date.now() + map.size).toString(32);
+			map.set(key, data);
+			return data;
+		},
+		set: (key, data) => map.set(key, data),
 		delete: (key) => map.delete(key),
 		find: (flt) => flt ? Array.from(map.values()).filter(flt) : Array.from(map.values()),
 		clear: () => map.clear()
 	}
 
-	// Add the validating layer
-	if (conf && typeof conf.validate === 'function') {
-		const set = store.set;
-		const validate = conf.validate;
-		store.set = (key, value) => {
-			validate(value);
-			return set(key, value);
-		}
-	}
-
-	store.create = store.set;
-
-	if (conf && conf.data) { // Add suplied data
-		Object.keys(conf.data).forEach(key => {
-			store.set(key, conf.data[key]);
-		});
-	}
-
 	return store;
 }
-
-module.exports = { create };
